c87a7b357fbc8621779ddeb46d73668b1f103941
xiaoyi <xiaoyi@xiaoyi.localdomain>
2024-06-06 00:11:41 +0800

    Finished 1-7 chapter.

---
+++ .history/exercises/move_semantics/move_semantics6_20240605235244.rs
@@ -0,0 +1,29 @@
+// move_semantics6.rs
+//
+// You can't change anything except adding or removing references.
+//
+// Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand
+// for a hint.
+
+// I AM NOT DONE
+
+fn main() {
+    let data = "Rust is great!".to_string();
+
+    let last_char = get_char(&data);
+    println!("The last character is {}", last_char);
+
+    string_uppercase(data);
+}
+
+// Should not take ownership
+fn get_char(data: &String) -> char {
+    data.chars().last().unwrap()
+}
+
+// Should take ownership
+fn string_uppercase(data: String) {
+    let data = data.to_uppercase();
+
+    println!("{}", data);
+}
